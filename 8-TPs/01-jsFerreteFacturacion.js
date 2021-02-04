7324c747e4eebf16130b0df07813a1a2624991c7
Laureano-Aguirre <laureanoaguirre2911@gmail.com>
2021-02-04 11:56:24 -0300

    Quinto commit

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -1,17 +1,112 @@
-/*1.	Para el departamento de facturación:
+/*1
+aguirre laureano
+TP 1
+.	Para el departamento de facturación:
 A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+    
+    //declaramos las variables
+    let precioUno;
+    let precioDos;
+    let precioTres;
+    let resultado;
+    let respuesta;
+
+    //definimos las variables
+    precioUno=document.getElementById('txtIdPrecioUno').value;
+    precioDos=document.getElementById('txtIdPrecioDos').value;
+    precioTres=document.getElementById('txtIdPrecioTres').value;
+
+    //transformamos el texto a numero
+    precioUno=parseFloat(precioUno);
+    precioDos=parseFloat(precioDos);
+    precioTres=parseFloat(precioTres);
+
+    //definimos la variable resultado
+    resultado=precioUno+precioDos+precioTres;
+
+    //definimos la variable respuesta
+    respuesta="la suma da " +resultado;
+
+    alert(respuesta);
+
+
+
+
 }
 function Promedio () 
 {
-	
+    
+    //declaramos las variables
+    let precioUno;
+    let precioDos;
+    let precioTres;
+    let resultado;
+    let respuesta;
+
+    //definimos las variables
+    precioUno=document.getElementById('txtIdPrecioUno').value;
+    precioDos=document.getElementById('txtIdPrecioDos').value;
+    precioTres=document.getElementById('txtIdPrecioTres').value;
+
+    //transformamos el texto a numero
+    precioUno=parseFloat(precioUno);
+    precioDos=parseFloat(precioDos);
+    precioTres=parseFloat(precioTres);
+
+    //definimos la variable resultado
+    resultado=(precioUno+precioDos+precioTres)/3;
+
+    //definimos la variable respuesta
+    respuesta="el promedio es " +resultado;
+
+    alert(respuesta);
+
+
+
+
 }
 function PrecioFinal () 
 {
-	
+    
+    //declaramos las variables
+    let precioUno;
+    let precioDos;
+    let precioTres;
+    let iva;
+    let resultadoInicial;
+    let resultadoFinal;
+    let respuesta;
+
+
+    //definimos las variables
+    precioUno=document.getElementById('txtIdPrecioUno').value;
+    precioDos=document.getElementById('txtIdPrecioDos').value;
+    precioTres=document.getElementById('txtIdPrecioTres').value;
+
+    //transformamos el texto a numeros
+    precioUno=parseFloat(precioUno);
+    precioDos=parseFloat(precioDos);
+    precioTres=parseFloat(precioTres);
+
+    //definimos la variable iva
+    iva=0.21;
+
+    //definimos la variable resultado inicial
+    resultadoInicial=precioUno+precioDos+precioTres;
+
+    //definimos la variable resultado final
+    resultadoFinal=(resultadoInicial*iva)+resultadoInicial;
+
+    //definimos la variable respuesta
+    respuesta="La operacion da " +resultadoFinal;
+
+    alert(respuesta);
+
+
+
 }
--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -7,12 +7,104 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 function Rectangulo () 
 {
 
+    //declaramos las variables
+    let largoDelTerreno;
+    let anchoDelTerreno;
+    let numeroDeHilos;
+    let resultado;
+    let respuesta;
+
+
+    //definimos las variables
+    largoDelTerreno=document.getElementById('txtIdLargo').value;
+    anchoDelTerreno=document.getElementById('txtIdAncho').value;
+    numeroDeHilos=3
+
+    //transformamos de texto a numeros
+    largoDelTerreno=parseFloat(largoDelTerreno);
+    anchoDelTerreno=parseFloat(anchoDelTerreno);
+
+    //definimos la variable resultado
+    resultado=((largoDelTerreno*anchoDelTerreno)*numeroDeHilos);
+
+    //definimos la variable respuesta
+    respuesta="Necesita comprar "+resultado+" metros de alambre";
+
+    alert(respuesta);
+
+
+
 }
 function Circulo () 
 {
-	
+    
+    //declaramos las variables
+    let radio;
+    let perimetro;
+    const PI=3.14;
+    let resultado;
+    let respuesta;
+
+
+
+    //definimos las variables
+    radio=document.getElementById('txtIdRadio').value;
+
+    //transformamos el texto a numeros
+    radio=parseFloat(radio);
+
+    //definimos la variable perimetro
+    perimetro=2 * PI * radio;
+
+    //definimos la variable resultado
+    resultado=perimetro*3;
+
+    //definimos la variable respuesta
+    respuesta="Necesita comprar "+resultado+" metros de alambre";
+
+    alert(respuesta);
+
+
+
+
+
 }
 function Materiales () 
 {
-	
+    
+    //declaramos las variables
+    let largoDelTerreno;
+    let anchoDelTerreno;
+    let area;
+    const CEMENTO_X_METRO=2;
+    const CAL_X_METRO=3;
+    
+
+    //definimos las variables
+    largoDelTerreno=document.getElementById('txtIdLargo').value;
+    anchoDelTerreno=document.getElementById('txtIdAncho').value;
+
+    //pasamos de texto a numeros
+    largoDelTerreno=parseFloat(largoDelTerreno);
+    anchoDelTerreno=parseFloat(anchoDelTerreno);
+
+    //definimos la variable area
+    area=largoDelTerreno*anchoDelTerreno;
+
+    //definimos la variable cal
+    cal=area*CAL_X_METRO;
+
+    //definimos la variable cemento
+    cemento=area*CEMENTO_X_METRO;
+
+    //definimos la variable respuesta
+    respuesta="Se necesitan "+cal+" bolsas de cal y "+cemento+" bolsas de cemento";
+    
+    alert(respuesta);
+
+
+
 }
+
+
+//siempre que tengamos una constante: se define cuando se declara la constante. Y al ser mas de una palabra, debemos separarlas por _
